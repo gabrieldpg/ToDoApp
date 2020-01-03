e63c871f65365fc846508379a05c08a33f6832b5
gabrieldpg <gabrielpcpg@gmail.com>
2020-01-04 11:37:23 +1300

    can now add item and delete item

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import ToDos from './ToDos';
+import Todos from './Todos';
+import AddTodo from './AddTodo';
 
 class App extends Component {
   state = {
-    todos: [
-      { id: 1, content: 'milk' },
-      { id: 2, content: 'sugar' }
-    ]
+    todos: []
   }
 
-  deleteToDo = (id) => {
+  deleteTodo = (id) => {
     const todos = this.state.todos.filter(todo => {
       return todo.id !== id;
     });
@@ -20,11 +18,20 @@ class App extends Component {
     
   }
 
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos: todos
+    });
+  }
+
   render() {
     return(
       <div className="todo-app container">
-        <h1 className="center blue-text">To Dos:</h1>
-          <ToDos todos={ this.state.todos } deleteToDo={ this.deleteToDo } />
+        <h1 className="center blue-text">Todos:</h1>
+        <Todos todos={ this.state.todos } deleteTodo={ this.deleteTodo } />
+        <AddTodo addTodo={ this.addTodo }/>
       </div>
     );
   }
--- src/ToDos.js
+++ src/ToDos.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const ToDos = ({todos, deleteToDo}) => {
+const Todos = ({todos, deleteTodo}) => {
     
     const todoList = todos.length ? (
         todos.map(todo => {
             return (
                 <div className="collection-item" key={ todo.id }>
-                    <span onClick={() => { deleteToDo(todo.id) }}>{ todo.content }</span>
+                    <span onClick={() => { deleteTodo(todo.id) }}>{ todo.content }</span>
                 </div>
             );
         })
@@ -21,4 +21,4 @@ const ToDos = ({todos, deleteToDo}) => {
     );
 }
 
-export default ToDos;
+export default Todos;
