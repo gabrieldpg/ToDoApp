60f37d5d36f3f13c6e238f071c2ce69f392dfc4d
gabrieldpg <gabrielpcpg@gmail.com>
2020-01-03 19:40:10 +1300

    added todo list and delete functionality with local data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ToDos from './ToDos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: 'milk' },
+      { id: 2, content: 'sugar' }
+    ]
+  }
+
+  deleteToDo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id;
+    });
+
+    this.setState({
+      todos: todos
+    });
+    
+  }
+
+  render() {
+    return(
+      <div className="todo-app container">
+        <h1 className="center blue-text">To Dos:</h1>
+          <ToDos todos={ this.state.todos } deleteToDo={ this.deleteToDo } />
+      </div>
+    );
+  }
 }
 
 export default App;
